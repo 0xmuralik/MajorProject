7e5b3e72c3de88bb9a5442abb24033fac429686e
SpideyPool <krishna192reddy@gmail.com>
2021-05-16 03:17:47 +0530

    fixed errors

--- Server/routes/folders.js
+++ Server/routes/folders.js
@@ -4,7 +4,7 @@ import { getFolders, createFolder} from '../controllers/folders.js'
 
 const router = express.Router();
 
-// http://localhost:5000/posts
+// http://localhost:5000/folders
 
 router.get('/',getFolders);
 router.post('/',createFolder);
