b09ea7c20f06f4e34dacc41e5a427a6628baee97
Murali Krishna Komatireddy <43192592+SpideyPool192@users.noreply.github.com>
2021-05-17 00:22:51 +0530

    created user schema and addded controller definitions

---
+++ Server/controllers/users.js
@@ -0,0 +1,12 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+import Users from '../models/users.js'
+
+export const signin = async (req,res) =>{
+
+}
+
+export const signup = async (req,res) =>{
+    
+}
---
+++ Server/models/users.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema({
+    name: {
+        type:String, 
+        required: true
+    },
+    email: {
+        type:String,
+        required:true
+    },
+    password: {
+        type:String,
+        required:true
+    },
+    username: {
+        type:String,
+        required:true
+    },
+    id:String
+})
+
+export default mongoose.model("Users",userSchema);
