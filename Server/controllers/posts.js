1ecc18fb3fe337b1f36e7df37155857e8c9ad0bb
SpideyPool <krishna192reddy@gmail.com>
2021-05-16 18:09:10 +0530

    added likePost controller body

--- Server/controllers/posts.js
+++ Server/controllers/posts.js
@@ -44,7 +44,14 @@ export const deletePost= async (req,res)=>{
 }
 
 export const likePost= async (req,res)=>{
+    const{id:_id} =req.params;
 
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+    const post = await PostMessage.findById(_id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id,{likeCount: post.likeCount+1},{new:true});
+
+    res.json(updatedPost);
 }
 
 export const savePost= async (req,res)=>{
