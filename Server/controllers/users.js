9f8e2aaf17e4aa33866164bc8a18359869937d1d
Murali Krishna Komatireddy <43192592+SpideyPool192@users.noreply.github.com>
2021-05-17 00:38:54 +0530

    added signin controller body

--- Server/controllers/users.js
+++ Server/controllers/users.js
@@ -4,7 +4,21 @@ import jwt from 'jsonwebtoken';
 import Users from '../models/users.js'
 
 export const signin = async (req,res) =>{
+    const {email , password} =req.body;
 
+    try {
+        const existingUser = await Users.findOne({email});
+        if(!existingUser) return res.status(404).json({message: "User doesn't exist."});
+
+        const isPasswordCorrect = await bcrypt.compare(password,existingUser.password);
+        if(!isPasswordCorrect) return res.status(400).json({message: "Incorrect Password."});
+
+        const token = jwt.sign({email:existingUser.email, id:existingUser._id,username:existingUser.username},'test',{expiresIn:'1h'});
+
+        res.status(200).json({result:existingUser,token});
+    } catch (error) {
+        res.status(500).json({ message:error});
+    }
 }
 
 export const signup = async (req,res) =>{
