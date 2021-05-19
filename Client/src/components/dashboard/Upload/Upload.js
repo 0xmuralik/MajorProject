c07680674fbca4d0551dd6e2c8ba9a1b7db5d88f
Priyaljain20 <priyal@gmail.com>
2021-05-19 07:35:54 +0530

    upload_page

--- Client/src/components/dashboard/Upload/Upload.js
+++ Client/src/components/dashboard/Upload/Upload.js
@@ -4,6 +4,11 @@ import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import Header from '../header/Header';
 import SidePannel from '../sidepannel/SidePannel';
+import Form from 'react-bootstrap/Form'
+import * as ViewData from '../Utils/ViewData'
+import Button from 'react-bootstrap/Button'
+import { Multiselect } from 'multiselect-react-dropdown';
+
 function Upload() {
     return (
 
@@ -19,106 +24,99 @@ function Upload() {
 
                         </Col>
                         <Col sm={10}>
-                        <div style={{ position: 'relative', top: '100px' }} class="page-wrapper bg-loginandregister p-t-130 p-b-100 font-poppins">
-                            <div class="wrapper wrapper--w680">
-                                        <h2 class="title">New Research</h2>
-                                        <form method="POST">
-                                            <div class="row row-space">
-                                                <div class="col-4">
-                                                    <div class="input-group">
-                                                        <label class="label">research title </label>
-                                                        <input class="input--style-4" type="text" name="title"/>
-                                                    </div>
-                                                </div>
+                            <div style={{ position: 'relative', top: '100px' }} class="page-wrapper bg-loginandregister p-t-130 p-b-100 font-poppins">
+                                <div class="wrapper wrapper--w680" style={{ width: '800px' }}>
+                                    <Form >
+                                        <Form.Group as={Row}>
+                                            <Form.Label column sm={2}>Title</Form.Label>
+                                            <Col sm={10}>
+                                                <Form.Control placeholder="Enter Title" />
+                                            </Col>
+                                        </Form.Group>
+                                        <Form.Group as={Row}>
+                                            <Form.Label column sm={2}>Author</Form.Label>
+                                            <Col sm={10}>
+                                                <Form.Control as="select">
+                                                    {ViewData.author_details.map((field) => (<option>{field.name}</option>))}
+                                                </Form.Control>
+                                            </Col>
+                                        </Form.Group>
+                                        <Form.Group as={Row}>
+                                            <Form.Label column sm={2}>Co-Authors</Form.Label>
+                                            <Col sm={10}>
+                                                <Multiselect
+                                                    options={ViewData.author_details} // Options to display in the dropdown
+                                                    selectedValues={ViewData.author_details.selectedValues}
+                                                    displayValue="name" // Property name to display in the dropdown options
+                                                /></Col>
+                                        </Form.Group>
+                                        <Form.Group as={Row}>
+
+                                            <Form.Label column sm={2}>Organization</Form.Label>
+                                            <Col sm={10}><Form.Control as="select">
+                                                <option>org 1</option>
+                                                <option>org 2</option>
+                                                <option>org 3</option>
+                                            </Form.Control></Col>
+                                        </Form.Group>
+                                        <Form.Group as={Row}>
+                                            <Form.Label column sm={2}>Region</Form.Label>
+                                            <Col sm={10}><Form.Control as="select">
+                                                <option>region 1</option>
+                                                <option>region 2</option>
+                                                <option>region 3</option>
+                                            </Form.Control></Col>
+
+                                        </Form.Group>
+                                        <Form.Group as={Row}>
+                                            <Form.Label column sm={2}>Domain</Form.Label>
+                                            <Col sm={10}><Form.Control as="select">
+                                                <option>AI/ML</option>
+                                                <option>Blockchain</option>
+                                                <option>Augmented Reality</option>
+                                            </Form.Control>
+                                            </Col>
+                                        </Form.Group>
+                                        <Form.Group as={Row}>
+                                            <div className="mb-3">
+                                                <Form.Check inline label="Pending" type='radio' name="group1" />
+                                                <Form.Check inline label="Completed" type='radio' name="group1" />
                                             </div>
-                                            <div class="row row-space">
-                                                <div class="col-4">
-                                                    <div class="input-group">
-                                                        <label class="label">description</label>
-                                                        <textarea class="input--style-4" type="text" name="description"></textarea>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="row row-space">
-                                                <div class="col-4">
-                                                    <div class="input-group">
-                                                        <label class="label">keywords - Optional</label>
-                                                        <input class="input--style-4" type="text" name="keywords"/>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="row row-space">
-                                                
-                                                <div class="col-4">
-                                                    <div class="input-group">
-                                                        <label class="label">Status</label>
-                                                        <div class="p-t-10">
-                                                            <label class="radio-container m-r-45">Completed
-                                                                <input type="radio"  name="gender"/>
-                                                                <span class="checkmark"></span>
-                                                            </label>
-                                                            <label class="radio-container">In Progress
-                                                                <input type="radio" name="gender"/>
-                                                                <span class="checkmark"></span>
-                                                            </label>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="input-group">
-                                                <label class="label">Domain</label>
-                                                <div class="rs-select2 js-select-simple select--no-search">
-                                                    <select name="domain">
-                                                        <option disabled="disabled" selected="selected">Choose option</option>
-                                                        <option>Subject 1</option>
-                                                        <option>Subject 2</option>
-                                                        <option>Subject 3</option>
-                                                    </select>
-                                                    <div class="select-dropdown"></div>
-                                                </div>
-                                            </div>
-                                            <div class="row row-space">
-                                                <div class="col-2">
-                                                    <div class="input-group">
-                                                        <label class="label">Organization</label>
-                                                        <div class="rs-select2 js-select-simple select--no-search">
-                                                            <select name="Organization">
-                                                                <option disabled="disabled" selected="selected">Choose option</option>
-                                                                <option>Add New</option>
-                                                                <option>Subject 2</option>
-                                                                <option>Subject 3</option>
-                                                            </select>
-                                                            <div class="select-dropdown"></div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="col-2">
-                                                    <div class="input-group">
-                                                        <label class="label">Region</label>
-                                                        <div class="rs-select2 js-select-simple select--no-search">
-                                                            <select name="region">
-                                                                <option disabled="disabled" selected="selected">Choose option</option>
-                                                                <option>Add New</option>
-                                                                <option>Subject 2</option>
-                                                                <option>Subject 3</option>
-                                                            </select>
-                                                            <div class="select-dropdown"></div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="p-t-15">
-                                                <button class="btn btn--radius-2 btn--blue" onclick="window.open('./projectHome.html')" type="submit">Submit</button>
-                                            </div>
-                                        </form>
+                                        </Form.Group>
+                                        <Form.Group as={Row}>
+                                            <Form.Label column sm={2}>Select images</Form.Label>
+                                            <Col sm={10}>
+                                                <Form.File
+                                                    id="custom-file"
+                                                    label="Select Images"
+                                                    custom
+                                                />
+                                            </Col>
+                                        </Form.Group>
+                                        <Form.Group as={Row}>
+                                            <Form.Label column sm={2}>Description</Form.Label>
+                                            <Col sm={10}><Form.Control as="textarea" rows={3} /></Col>
+                                        </Form.Group>
+                                        <Form.Group as={Row}>
+                                            <Form.Label column sm={2}>Work Done</Form.Label>
+                                            <Col sm={10}><Form.Control as="textarea" rows={3} /></Col>
+                                        </Form.Group>
+                                        <Form.Group as={Row}>
+                                            <Form.Label column sm={2}>Future Work</Form.Label>
+                                            <Col sm={10}><Form.Control as="textarea" rows={3} /></Col>
+                                        </Form.Group>
+                                        <Button variant="primary" type="submit">
+                                            Submit
+                </Button>
+                                    </Form>
+                                </div>
                             </div>
-                        </div>
                         </Col>
                     </Row>
                 </Container>
             </div>
         </>
-       
+
     )
 }
 
